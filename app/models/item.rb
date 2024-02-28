620937818f03fd25cc3aff9c3ad42ba6cd3ca460
buhibuhiRich <buhibuhi591@gmail.com>
2024-02-28 21:23:56 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
  
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
   belongs_to :condition
   belongs_to :shipping_cost_responsibility
   belongs_to :shipping_from_region
@@ -12,9 +13,8 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true }
-  validates :condition_id, presence: true
-  validates :shipping_cost_responsibility_id, presence: true
-  validates :shipping_from_region_id, presence: true
-  validates :days_until_shipment_id, presence: true
-  validates :id, exclusion: { in: [1], message: "ID 1は選択できません" }
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_responsibility_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_from_region_id, numericality:{ other_than: 1 , message: "can't be blank"}
+  validates :days_until_shipment_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
