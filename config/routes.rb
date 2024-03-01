d379ef4443848f945d16152e3ef592beced08ba1
buhibuhiRich <buhibuhi591@gmail.com>
2024-03-02 02:02:19 +0900

    機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:new, :create, :index ]
+  resources :items, only: [:new, :create, :index, :show, :edit, :update] do
+    member do
+      get 'purchase'  
+    end
+  end
   get '/new_item', to: 'items#new', as: 'new_item_pass'
-
-
   get '/new_item_page', to: 'items#new', as: 'new_item_page'
 end
