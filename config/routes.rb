18267bb852ac8a7f39a872751872db1721aec1c9
buhibuhiRich <buhibuhi591@gmail.com>
2024-03-02 22:17:28 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:new, :create, :index ]
-  get '/new_item', to: 'items#new', as: 'new_item_pass'
-
-
-  get '/new_item_page', to: 'items#new', as: 'new_item_page'
+  resources :items, only:[:new, :create, :index, :show, :edit, :update, :destroy]
 end
