cba23d82abe4d95654571f2062e5a00a42199289
buhibuhiRich <buhibuhi591@gmail.com>
2024-03-03 16:28:57 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,8 +19,8 @@ class Item < ApplicationRecord
   validates :shipping_from_region_id, numericality:{ other_than: 1 , message: "can't be blank"}
   validates :days_until_shipment_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-  def sold_out
+  # def sold_out
   
-    price <= 0
-  end
+  #   price <= 0
+  # end
 end
