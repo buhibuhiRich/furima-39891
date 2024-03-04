e438a66bb897601ba97a397c6c995cd00f862925
buhibuhiRich <buhibuhi591@gmail.com>
2024-03-04 14:03:21 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,6 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
-      puts @item.errors.full_messages
       render :edit, status: :unprocessable_entity
     end
   end
